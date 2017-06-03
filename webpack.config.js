b8b1aab5aa4d9f54ab41f5c79f588fbc9b73f0f2
Nahuel Jesús Sacchetti <nahueljsacchetti@gmail.com>
2017-06-03 13:28:52 -0300

    Add main.js with React, config loaders for webpack, add index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
-var Webpack = require('webpack');
+'use strict';
 
-const BUILD_DIR = '/dist';
-const UI_DIR = '/ui';
-const config = {
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
+var BUILD_DIR = path.join(__dirname, '/dist/');
+var EXCLUDE_FOLDERS = [
+    /node_modules/,
+    /dist/
+];
+var UI_DIR = path.join(__dirname, '/ui/');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        path.join(__dirname, 'main.js')
+    ],
+    output: {
+        path: BUILD_DIR,
+        filename: [name].js,
+        publicPath: '/'
+    },
+    plugins: [
+
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: EXCLUDE_FOLDERS,
+                loader: 'babel-loader'
+            },
+            {
+                // TODO: Run plugin to extract this CSS.
+                test: /\.scss$/,
+                exclude: EXCLUDE_FOLDERS,
+                loader: 'style-loader!css-loader?module&localIdentName=[name]---[local]---[hash:base64:5]!sass-loader'
+            }
+        ]
+    }
 };
-
-module.exports = config;
